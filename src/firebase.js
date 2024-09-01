83a053a2743ec9857998d2999871d266122c96c4
Not-7Myself <98397618+Not-7Myself@users.noreply.github.com>
2024-09-01 21:31:19 +0530

    safe maybe

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,20 @@
 import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDukU1s-TywFMEc8tIyqT1v7fcyP_Hvdkg",
-  authDomain: "backend-test-cbc8c.firebaseapp.com",
-  projectId: "backend-test-cbc8c",
-  storageBucket: "backend-test-cbc8c.appspot.com",
-  messagingSenderId: "695881769295",
-  appId: "1:695881769295:web:fb3d0496ad5725a878653a",
-  measurementId: "G-WB4S0KWQQ3",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore();
+firebase.initializeApp(firebaseConfig);
 
-export { db };
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
